7c57e1cbc58e03d8af20bd04e7616f358a34e7e6
lukecanny <lukecanny@protonmail.ch>
2022-10-11 14:04:26 +0100

    Added comments to Lecturer.java

--- lib/src/main/java/assignment1_ct417/Lecturer.java
+++ lib/src/main/java/assignment1_ct417/Lecturer.java
@@ -2,7 +2,11 @@ package assignment1_ct417;
 
 import org.joda.time.*;
 import java.util.ArrayList;
-
+/* The Lecturer class is responsible for storing all information relating to an individual lecturer. 
+ * This includes name, date of birth, staff id, modules they are responsible for.
+ * @author Luke Canny 19339166
+ * @date 11-10-22
+ */
 public class Lecturer implements UniversityElement {
 	
 	String name;
@@ -11,44 +15,53 @@ public class Lecturer implements UniversityElement {
 	static int lastIdUsed = 0;
 	ArrayList<Module> listOfTaughtModules = new ArrayList<Module>();
 	
+	// Constructor takes name and date of birth as fields
 	public Lecturer (String name, LocalDate dob){
 		this.name = name;
 		this.dob = dob;
-		id = ++lastIdUsed;
+		id = ++lastIdUsed;	// New Staff ID assigned here
 	}
 	
+	// Returns user name of lecturer (first 5 letters of name and age)
 	public String getUsername() {
-		return name.replaceAll("//s+", "").substring(0,5) + getAge();
+		return name.replaceAll(" ", "").substring(0,5) + getAge();
 	}
 	
+	// Returns age
 	public int getAge() {
 		LocalDate currentDate = new LocalDate();
 	    Years age = Years.yearsBetween(dob,currentDate);
 	    return age.getYears();
 	}
 	
+	// returns date of birth
 	public LocalDate getDateOfBirth() {
 		return dob;
 	}
 	
+	// returns staff id
 	public int getID() {
 		return id;
 	}
 	
+	// returns lecturer's name
 	public String getName() {
 		return name;
 	}
 
+	// Adds module to list of taught modules
 	public void addModule(Module mod) {
 		listOfTaughtModules.add(mod);
 	}
 
+	// Adds multiple modules to list of taught modules
 	public void addModules(ArrayList<Module> mods) {
 		for (int i = 0; i < mods.size(); i++) {
 			listOfTaughtModules.add(mods.get(i));
 		}
 	}
 	
+	// Removes a module from the arraylist listOfTaughtModules
 	public void removeModule(Module mod) {
 		for (int i = 0; i < listOfTaughtModules.size(); i++) {
 			if (mod.equals(listOfTaughtModules.get(i))) {
@@ -57,10 +70,13 @@ public class Lecturer implements UniversityElement {
 		}
 	}
 	
+	// Returns arraylist of modules taught by lecturer
 	public ArrayList<Module> getListOfModules(){
 		return listOfTaughtModules;
 	}
 	
+	// Overridden toString 
+	@Override
 	public String toString() {
 		String str = "";
 		str += "Name: " + getName();
