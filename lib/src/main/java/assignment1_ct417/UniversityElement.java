50db44f6e1c036b14ccfc59f01ffadd1e50a41f3
lukecanny <lukecanny@protonmail.ch>
2022-10-11 14:15:40 +0100

    Added comments to UniversityElement.java interface

--- lib/src/main/java/assignment1_ct417/UniversityElement.java
+++ lib/src/main/java/assignment1_ct417/UniversityElement.java
@@ -1,5 +1,6 @@
 package assignment1_ct417;
 
+// This interface must be implemented by all university objects. (This class is required to avoid repetitive code in Driver class)
 public interface UniversityElement {
 	public String getName();	// Returns name
 }
